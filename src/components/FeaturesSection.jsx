4f455694b46dcc25bd177951a99ecf8ac80b13cd
rahultherocks67 <eoffice.rahul@gmail.com>
2025-07-13 18:03:37 +0530

    hero-updation for medium and above devices

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -15,7 +15,7 @@ const FeaturesSection = () => {
   return (
     <section className="bg-gray-100">
       <div className="container max-w-screen-xl mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {features.map((feature, index) => (
             <FeatureCard
               key={index}
