7ccfd0f12b05a84c9855e547318a888edb1bcf45
rahultherocks67 <eoffice.rahul@gmail.com>
2025-07-13 18:08:35 +0530

    hero-updation for medium and above devices

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
   
       <h2 className='text-[min(5vw,2rem)] font-semibold text-center mx-auto max-w-7xl p-1 '>Avail The Best Chauffeur Service Now!</h2>
 
-      <div className="relative min-h-[60vh] sm:min-h-[40vh] sm:max-h-screen-md w-full my-auto">
+      <div className="relative min-h-[60vh] sm:min-h-[40vh] sm:max-h-[70vh] w-full my-auto">
       <img
         src={heroimage}
         alt="Hero"
@@ -17,7 +17,7 @@ const HeroSection = () => {
 
   <div class="grid grid-cols-1 md:grid-cols-2 gap-4 h-full">
     {/* First Block */}
-    <div class="hidden md:block p-4">
+    <div class="hidden w-full h-full md:block p-4">
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-4">
         <h1 className="text-[min(10vw,48px)] font-bold">Welcome to Our Site</h1>
         <p className="mt-4 text-[min(10vw,18px)] max-w-xl">
@@ -29,7 +29,7 @@ const HeroSection = () => {
       </div>
       </div>
     {/* Second Block */}
-       <div class="block p-2 md:p-4">
+       <div class="block p-2 w-full h-full md:p-4">
         <div className="relative z-10 flex flex-col items-center justify-center m-auto h-full text-white text-center mx-1 md:p-4 md:m-auto">
         <Tabs />
         </div>  
